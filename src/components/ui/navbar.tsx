c943ed065cc770e6aeb58df8973b61cc7cd12e73
kopperino <181644388+kopperino@users.noreply.github.com>
2025-03-19 21:00:50 +1100

    Navbar update

--- src/components/ui/navbar.tsx
+++ src/components/ui/navbar.tsx
@@ -7,51 +7,30 @@ export default function Navbar() {
 
     return (
         <nav className="bg-gradient-to-r from-gray-900 to-gray-800 text-white font-bold text-lg">
-            <div className="container mx-auto flex justify-center items-center p-4">
-                <button
-                    className="md:hidden"
-                    onClick={() => setIsOpen(!isOpen)}
-                >
-                    {isOpen ? <X size={28} /> : <Menu size={28} />}
-                </button>
-
-                <ul
-                    className={`
-                    absolute md:static left-0 top-16 w-full md:w-auto bg-gray-900 md:bg-transparent 
-                    flex flex-col md:flex-row md:items-center md:gap-8 gap-4 text-center md:text-left 
-                    transition-all duration-300 ease-in-out ${
-                        isOpen ? 'block' : 'hidden md:flex'
-                    }
-                `}
-                >
+            <div className="flex justify-center items-center p-6">
+                <ul className="flex flex-col md:flex-row gap-10">
                     <li>
-                        <Link
-                            href="/extraction"
-                            className="block py-2 px-4 hover:bg-gray-700 rounded"
-                        >
+                        <Link href="/extraction" className="hover:underline">
                             Extraction
                         </Link>
                     </li>
                     <li>
                         <Link
                             href="/classification"
-                            className="block py-2 px-4 hover:bg-gray-700 rounded"
+                            className="hover:underline"
                         >
                             Classification
                         </Link>
                     </li>
                     <li>
-                        <Link
-                            href="/summarisation"
-                            className="block py-2 px-4 hover:bg-gray-700 rounded"
-                        >
+                        <Link href="/summarisation" className="hover:underline">
                             Summarisation
                         </Link>
                     </li>
                     <li>
                         <Link
                             href="/semanticsearchengine"
-                            className="block py-2 px-4 hover:bg-gray-700 rounded"
+                            className="hover:underline rounded"
                         >
                             Semantic Search
                         </Link>
@@ -59,7 +38,7 @@ export default function Navbar() {
                     <li>
                         <Link
                             href="/retrievalaugmentedgeneration"
-                            className="block py-2 px-4 hover:bg-gray-700 rounded"
+                            className="hover:underline"
                         >
                             Retrieval Augmented Generation
                         </Link>
@@ -67,7 +46,7 @@ export default function Navbar() {
                     <li>
                         <Link
                             href="/syntheticdatageneration"
-                            className="block py-2 px-4 hover:bg-gray-700 rounded"
+                            className="hover:underline"
                         >
                             Synthetic Data Generation
                         </Link>
